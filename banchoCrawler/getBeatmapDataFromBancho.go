45308406e55fa40acf79597d2d6026e3d37f5a17
thftgr <thftgr@gmail.com>
2023-06-22 23:36:20 +0900

    삭제된 맵 갱신 오류 수정

--- banchoCrawler/getBeatmapDataFromBancho.go
+++ banchoCrawler/getBeatmapDataFromBancho.go
@@ -512,10 +512,15 @@ func updateSearchBeatmaps(data []osu.BeatmapSetsIN) (err error) {
 		Query: fmt.Sprintf(mapUpsert, buildSqlValues(mapValues, len(beatmaps))),
 		Args:  mapInsertBuf,
 	}
+	db.InsertQueueChannel <- db.ExecQueue{
+		//삭제된맵 삭제
+		Query: `/* SOFT DELETE MAP */ UPDATE BEATMAP SET DELETED_AT = CURRENT_TIMESTAMP WHERE  BEATMAPSET_ID IN  @mapSets AND BEATMAP_ID NOT IN @maps;`,
+		Args:  []any{sql.Named("mapSets", beatmapSets), sql.Named("maps", beatmaps)},
+	}
 
 	db.InsertQueueChannel <- db.ExecQueue{
 		//삭제된맵 삭제
-		Query: ` /* SOFT DELETE MAP */ UPDATE BEATMAP SET DELETED_AT = CURRENT_TIMESTAMP 
+		Query: ` /* SOFT DELETE MAP */ UPDATE BEATMAPSET SET DELETED_AT = CURRENT_TIMESTAMP 
                       WHERE  BEATMAPSET_ID IN (
 					      SELECT MS.BEATMAPSET_ID FROM BEATMAPSET MS LEFT JOIN BEATMAP M on MS.BEATMAPSET_ID = M.BEATMAPSET_ID
 						WHERE TRUE
